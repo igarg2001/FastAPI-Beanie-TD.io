0d28219f100a811146049e42957a9111e888974d
Ishan Garg <igarg2001@gmail.com>
2023-04-05 18:30:41 +0530

    add read api

--- app/server/routes/product_review.py
+++ app/server/routes/product_review.py
@@ -10,3 +10,13 @@ router = APIRouter()
 async def add_product_review(review: ProductReview) -> dict:
     await review.create()
     return {'message': 'Review added successfully'}
+
+@router.get('/{id}', response_description='review record corresponding to ID retrieved')
+async def get_review_by_id(id: PydanticObjectId) -> ProductReview:
+    review = await ProductReview.get(id)
+    return review
+
+@router.get('/', response_description='all review records retrieved')
+async def get_all_reviews() -> List[ProductReview]:
+    reviews = await ProductReview.find_all().to_list()
+    return reviews
